85bf846c827a69598721f5c3fc79884201883e8f
maxherbst <mxh1985@gmail.com>
2024-04-29 13:03:57 +0200

    mbmh site udpate

--- src/App.jsx
+++ src/App.jsx
@@ -50,89 +50,33 @@ export default function App() {
         enabled={enabled}
         config={{ syncTouch: true }} // Lenis setting to force smooth scroll on touch devices
       />
+
+      <header className="top-header">
+        <span className="logo">MB/MH</span>
+        <span className="info">Info</span>
+      </header>
+
+      <dialog className="intro center-flex">
+          <span className="headline">Branding & Digital</span>
+          <h1>MB/MH</h1>
+      </dialog>
+
+      <section className="container first-section">
+          <Image
+            src="images/colombe.jpg"
+            className="IntroImage"
+            parallaxSpeed={1.08}
+          />
+      </section>
+
       <article>
         <header className="container">
           <div className="headerLayout">
             <h2>
-              <Headline wobble>
-                MB/MH STUDIO
-              </Headline>
+              <Headline wobble>We are a branding and digital centered creative studio based in Warsaw, working worldwide. </Headline>
             </h2>
-            <BodyCopy as="p" className="subline">
-              Progressively enhance your React website with WebGL using
-              r3f-scroll-rig, React Three Fiber and Three.js
-            </BodyCopy>
           </div>
         </header>
-        <section className="container">
-          <Image
-            src="images/maxim-berg-1_U2RcHnSjc-unsplash.jpg"
-            className="ImageLandscape"
-          />
-        </section>
-        <section className="container">
-          <h3>
-            <Subtitle>We use CSS to create a responsive layout.</Subtitle>
-            <em>
-              <Subtitle>
-                A Canvas on top tracks DOM elements and enhance them with WebGL.
-              </Subtitle>
-            </em>
-          </h3>
-          <p>
-            <BodyCopy>
-              Try turning off WebGL using the button in the sticky header.
-              You’ll notice smooth scrolling is disabled, and all scroll-bound
-              WebGL effects disappears.
-            </BodyCopy>
-          </p>
-        </section>
-        <section className="ParallaxContainer">
-          <Image
-            src="images/maxim-berg-qsDfqZyTCAE-unsplash-crop.jpg"
-            className="aspect-9_13"
-            parallaxSpeed={1.08}
-          />
-          <Image
-            src="images/maxim-berg-ANuuRuCRRAc-unsplash.jpg"
-            className="aspect-16_11"
-            parallaxSpeed={0.92}
-          />
-        </section>
-        <section className="container">
-          <h4>
-            <BodyCopy>
-              Thanks to Threejs we can also render 3D geometry or models. The
-              following image is replaced by a box. Try scrolling hard to make
-              it wiggle.
-            </BodyCopy>
-          </h4>
-        </section>
-        <section>
-          <ImageCube
-            src="images/maxim-berg-TcE45yIzJA0-unsplash.jpg"
-            className="JellyPlaceholder"
-          />
-        </section>
-        <section className="container">
-          <h3>
-            <Subtitle>Most websites use a mix of WebGL and HTML.</Subtitle>
-            <em>
-              <Subtitle>
-                However, the Lens refraction requires all images and text to be
-                WebGL.
-              </Subtitle>
-            </em>
-          </h3>
-          <p>
-            <a href="https://github.com/14islands/r3f-scroll-rig">
-              <BodyCopy>
-                You can find the r3f-scroll-rig library on Github. Please use
-                WebGL responsibly™.
-              </BodyCopy>
-            </a>
-          </p>
-        </section>
         <footer>
           <CodropsFrame />
         </footer>
